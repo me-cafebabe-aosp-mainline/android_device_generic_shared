779ed289ab71bd0d7048d3a36b6596e705ce86ae
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-01 23:54:24 +0800

    features: Add serial console as root
    Change-Id: I67642cf5ba0d5aeb3b48ef437a7ab025fd57f520

---
+++ features/serial_console_as_root/serial_console_as_root.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/serial_console_as_root.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/serial_console_as_root.rc \
+    $(LOCAL_PATH)/serial_console_as_root.rc:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/init/serial_console_as_root.rc
