fec2f39f60f1c0bfbe582ac0940f7ccff6f51db8
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-04 00:34:32 +0800

    hw: templates: health: Add template for example health aidl
    Change-Id: Ieae9f2b9f3dde26684bf3623deca0f193a49e619

---
+++ hardware/templates/health/example-aidl.mk
@@ -0,0 +1,6 @@
+# Reference: hardware/interfaces/health/aidl/README.md
+
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery \
+    charger_res_images_vendor
